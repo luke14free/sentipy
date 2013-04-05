dd11e7f261dd1e9e963673b07c512010e24bf4a2
Luca Giacomel <luke14free@gmail.com>
2013-04-05 21:07:32 +0200

    Added compatibility with python 2.6

--- custom_classifier.py
+++ custom_classifier.py
@@ -13,11 +13,16 @@ from sklearn.preprocessing import Scaler
 from sklearn.pipeline import Pipeline
 from sklearn.externals import joblib
 from sklearn.feature_extraction.text import CountVectorizer
-from matplotlib.mlab import PCA
-from collections import Counter
+from matplotlib.mlab import PCA    
 import matplotlib.pyplot as plt
 import pylab as pl
 import numpy as np
+
+try:
+    from collections import Counter
+except ImportError:
+    from collections_fallback import Counter
+
 
 stopwords = ['rt','re',':(',':)',':))',':((','http','im','i', 'me', 'my', 'myself', 'we', 'our', 'ours', 'ourselves', 'you', 'your', 'yours', 'yourself', 'yourselves', 'he', 'him', 'his', 'himself', 'she', 'her', 'hers', 'herself', 'it', 'its', 'itself', 'they', 'them', 'their', 'theirs', 'themselves', 'what', 'which', 'who', 'whom', 'this', 'that', 'these', 'those', 'am', 'is', 'are', 'was', 'were', 'be', 'been', 'being', 'have', 'has', 'had', 'having', 'do', 'does', 'did', 'doing', 'a', 'an', 'the', 'and', 'but', 'if', 'or', 'because', 'as', 'until', 'while', 'of', 'at', 'by', 'for', 'with', 'about', 'against', 'between', 'into', 'through', 'during', 'before', 'after', 'above', 'below', 'to', 'from', 'up', 'down', 'in', 'out', 'on', 'off', 'over', 'under', 'again', 'further', 'then', 'once', 'here', 'there', 'when', 'where', 'why', 'how', 'all', 'any', 'both', 'each', 'few', 'more', 'most', 'other', 'some', 'such', 'no', 'nor', 'not', 'only', 'own', 'same', 'so', 'than', 'too', 'very', 's', 't', 'can', 'will', 'just', 'don', 'should', 'now']
 
